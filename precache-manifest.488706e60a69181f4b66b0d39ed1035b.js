f1b9caba67aac8b1dab78a39dae7dd80bda7bd56
Tomi Heikkinen <heikkinentomi@hotmail.com>
2020-04-20 13:06:22 +0300

    Updates

---
+++ precache-manifest.488706e60a69181f4b66b0d39ed1035b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8b447c39d47e0b0fad4dafe72b601d",
+    "url": "/temperamenttiKysely/index.html"
+  },
+  {
+    "revision": "032bce00481d4c7570b6",
+    "url": "/temperamenttiKysely/static/css/main.0b3d27f6.chunk.css"
+  },
+  {
+    "revision": "3e8685a203699d8cfdab",
+    "url": "/temperamenttiKysely/static/js/2.9008e664.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temperamenttiKysely/static/js/2.9008e664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032bce00481d4c7570b6",
+    "url": "/temperamenttiKysely/static/js/main.69880510.chunk.js"
+  },
+  {
+    "revision": "4bf075b627ec39e91527",
+    "url": "/temperamenttiKysely/static/js/runtime-main.3f093148.js"
+  }
+]);
